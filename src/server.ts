adf16054adfc1dbd33913ecdcc7475f08a73d878
Lucas Palharini <lucas230991@hotmail.com>
2022-10-06 23:29:58 -0300

    feat: updated for use on AWS

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
-import app from "./app.js";
+import app from './app.js';
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log(`Servidor funfando na porta: ${port}`);
-})
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Servidor funfando na porta: ${PORT}`);
+});
